1d19aa56f7f3c916c22d8c35a8eb933d1adf994f
Alexandr Shestakov <akaSybe@gmail.com>
2017-11-26 02:28:09 +0300

    update library version

--- src/Disqus.NET/Properties/AssemblyInfo.cs
+++ src/Disqus.NET/Properties/AssemblyInfo.cs
@@ -7,8 +7,8 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 
 [assembly: AssemblyTitle("Disqus.NET")]
-[assembly: AssemblyVersion("1.1.0")]
-[assembly: AssemblyFileVersion("1.1.0")]
+[assembly: AssemblyVersion("1.0.1")]
+[assembly: AssemblyFileVersion("1.0.1")]
 
 [assembly: InternalsVisibleTo("Disqus.NET.Tests")]
 
